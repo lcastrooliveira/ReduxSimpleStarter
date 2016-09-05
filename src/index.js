e6e67ff294c49f34327ae9f3010b25c858243626
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-05 14:34:08 +0000

    Section 1, Lecture 14

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as keys from './key';
+import SearchBar from './components/search_bar';
+
+const API_KEY = keys.API_KEY;
 
 //Create a new component. This component should produce some HTML.
 //const is a keyword in ES6 that is equal a final variable in java, quite similar
@@ -10,7 +13,11 @@ import * as keys from './key';
 const App = () => {
     //JSX is a dialect of JS that allows to type HTML inside JS, but the transpiler
     //will convert to ES5 javascript when providing to the browser.
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //Take this component's generate HTML and put it on the page (in the DOM)
