30c272f1871b41559d559a946e88ac22c8077fd0
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-05 14:57:58 +0000

    Section 1, Lecture 16 part I

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+//{} just pull out Component from react. Very similart to java's static import
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input/>
+//Functional component: components made out of functions.
+
+//Class component: component that are aware of itself and more complex.
+//It is a javascript object with properties and methods
+
+//Gives access of all functionality from React.Component class
+class SearchBar extends Component {
+    //every class based component MUST have defined a render() method
+    render() {
+        //link event from component to the handler
+        return <input onChange={this.onInputChange}/>;
+    }
+    
+    //to handle an event
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
 }
+
 
 export default SearchBar;
