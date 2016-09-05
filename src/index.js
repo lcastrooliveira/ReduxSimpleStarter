da9acfb93124916d57203954e023be4be85a2e85
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-05 18:02:23 +0000

    Section 2, Lecture 22

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -7,23 +7,32 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = keys.API_KEY;
 
-YTSearch({key: API_KEY, term: 'surfboards'},function(data) {
-    console.log(data);
-});
-
 //Create a new component. This component should produce some HTML.
 //const is a keyword in ES6 that is equal a final variable in java, quite similar
 // () => replaces function()
 
 //Rule of thumb: Always one components per file
-const App = () => {
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+            //equals to this.setState({videos: videos});
+        });
+    }
+    
     //JSX is a dialect of JS that allows to type HTML inside JS, but the transpiler
     //will convert to ES5 javascript when providing to the browser.
-    return (
+    render() {
+        return (
         <div>
             <SearchBar />
         </div>
-    );
+        );    
+    }
 }
 
 //Take this component's generate HTML and put it on the page (in the DOM)
