01aeeac0171c6021e167d7fa279aa34f1c344743
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-05 13:53:08 +0000

    Section 1, Lecture 11

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 //Create a new component. This component should produce some HTML.
 //const is a keyword in ES6 that is equal a final variable in java, quite similar
-
-const App = function() {
+// () => replaces function()
+const App = () => {
     //JSX is a dialect of JS that allows to type HTML inside JS, but the transpiler
     //will convert to ES5 javascript when providing to the browser.
     return <div>Hi!</div>;
 }
 
 //Take this component's generate HTML and put it on the page (in the DOM)
-ReactDOM.render(<App/>);
+ReactDOM.render(<App/>, document.querySelector('.container'));
