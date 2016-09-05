32217713e8c9fce987272a2733281e5d6682caa7
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-05 17:52:44 +0000

    Section 2, Lecture 21

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import * as keys from './key';
 import SearchBar from './components/search_bar';
 
 const API_KEY = keys.API_KEY;
+
+YTSearch({key: API_KEY, term: 'surfboards'},function(data) {
+    console.log(data);
+});
 
 //Create a new component. This component should produce some HTML.
 //const is a keyword in ES6 that is equal a final variable in java, quite similar
