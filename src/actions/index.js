ec984a8ff79151d0127260cf2f7dc23b962949a4
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-07 19:04:33 +0000

    Section 5, Lecture 56

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import * as keys from '../key';
+
+const ROOT_URL = `http://api.openweather.org/data/2.5/forecast?appid=${keys.API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&${city},br`;
+    //returns a promisse
+    const request = axios.get(url);    
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
