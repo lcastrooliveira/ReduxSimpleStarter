330821310f23ef812149430189afac80a4645b21
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-07 19:18:39 +0000

    Section 5, Lecture 57

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 import * as keys from '../key';
 
-const ROOT_URL = `http://api.openweather.org/data/2.5/forecast?appid=${keys.API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${keys.API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&${city},br`;
+    const url = `${ROOT_URL}&q=${city},br`;
     //returns a promisse
     const request = axios.get(url);    
     return {
