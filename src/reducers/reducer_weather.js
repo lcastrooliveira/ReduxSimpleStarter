b04cfb02ae8b688622553e6a025439981681c186
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-08 03:14:54 +0000

    Section 5, Lecture 59

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action) {
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+            //this is a can of worms dont ever manipulate state directly
+            //return state.push(action.payload.data);
+            //use this instead(create an entirely new array)
+            //return state.concat([action.payload.data]);
+            //below is the same thing but with ES6 syntax sugar
+            return [action.payload.data,...state];
+    }
     return state;
 }
