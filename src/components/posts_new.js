3db73bdfe984625b725d2f76530c996fae0ccd7e
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-09 04:23:07 +0000

    Section 6, Lecture 88

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
     render() {
@@ -9,22 +10,29 @@ class PostsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+                <div className= {`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title}/>
                     <div className="text-help">
                         {title.touched ? title.error : ''}
                     </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories}/>
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content}/>
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Save</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -34,6 +42,12 @@ function validate(values) {
     const errors = {};
     if(!values.title) {
         errors.title = 'Enter a username';
+    }
+    if(!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+    if(!values.content) {
+        errors.content = 'Enter some content';
     }
     return errors;
 }
