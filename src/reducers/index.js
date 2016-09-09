0bc4ce0db26f4f176ad2409d770b508524f14847
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-09-09 02:49:53 +0000

    Section 6, Lecture 83

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import PostReducer from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  //make sure you use the key 'form'
+  form: formReducer
 });
 
 export default rootReducer;
